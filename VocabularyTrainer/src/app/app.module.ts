b8df004bab3ecd59de5b876d2b8fcdb30531c4b7
Ahmad <60389579+ahmadjafari86@users.noreply.github.com>
2021-10-08 18:09:12 +0330

    commit-2

--- VocabularyTrainer/src/app/app.module.ts
+++ VocabularyTrainer/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
