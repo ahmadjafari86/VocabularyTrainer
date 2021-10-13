9ea56255d71388e20b3479d71342d7af8d52d759
Ahmad <60389579+ahmadjafari86@users.noreply.github.com>
2021-10-13 23:55:50 +0330

    commit-10

--- VocabularyTrainer/src/app/components/wordtraining/wordtraining.component.ts
+++ VocabularyTrainer/src/app/components/wordtraining/wordtraining.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs/internal/operators";
 
 @Component({
   selector: 'app-wordtraining',
@@ -7,17 +8,40 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./wordtraining.component.css']
 })
 export class WordtrainingComponent implements OnInit {
-  word = "";
-  englishMeaning = "";
+  loadedPosts=[];
   constructor(private http:HttpClient) { }
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData:{word:string;englishMeaning:string}){
     this.http.post('https://vocabulary-trainer-2021-default-rtdb.firebaseio.com/posts.json',
       postData).subscribe(responseData=>{console.log(responseData);
       });
   }
-
-  ngOnInit(): void {
+  onFetchPosts(){
+     this.fetchPosts();
   }
+
+  onClearPosts(){
+
+  }
+
+  private fetchPosts(){
+    this.http.get('https://vocabulary-trainer-2021-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(responseData=>{
+        const postsArray =[];
+        for (const key in responseData){
+          if (responseData.hasOwnProperty(key)){
+            // @ts-ignore
+            postsArray.push({...responseData[key],id:key});
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(posts=>{console.log(posts);
+    });
+  }
+
 
 }
